fc700cfdba9eca4a678d6f767d516981e17950f1
Akihiro Harai <jharai0815@gmail.com>
2015-09-05 16:49:44 +0900

    Add "commit" command

--- main.go
+++ main.go
@@ -1,31 +1,67 @@
 package main
 
+// import (
+// 	"fmt"
+// 	"os"
+//
+// 	"github.com/harai/hubbit/git"
+// 	"github.com/harai/hubbit/github"
+// 	"github.com/harai/hubbit/hub"
+// 	"github.com/octokit/go-octokit/octokit"
+// )
+//
+// func main() {
+// 	issue, err := git.CurrentIssueNo()
+// 	if err != nil {
+// 		fmt.Println("not in an issue branch")
+// 		os.Exit(1)
+// 	}
+// 	fmt.Println(github.IssueAsHashtag(issue))
+// 	client := hub.Authenticate()
+// 	url, err := octokit.CurrentUserURL.Expand(octokit.M{})
+// 	if err != nil {
+// 		panic("Not happen")
+// 	}
+// 	fmt.Println(url)
+// 	user, result := client.Users(url).One()
+// 	if result.Err != nil {
+// 		panic(result.Response.Status)
+// 	}
+// 	fmt.Println(user.ReposURL)
+// }
+
 import (
-	"fmt"
+	"log"
 	"os"
 
+	"github.com/codegangsta/cli"
 	"github.com/harai/hubbit/git"
 	"github.com/harai/hubbit/github"
-	"github.com/harai/hubbit/hub"
-	"github.com/octokit/go-octokit/octokit"
 )
 
-func main() {
+func commit(c *cli.Context) {
 	issue, err := git.CurrentIssueNo()
 	if err != nil {
-		fmt.Println("not in a issue branch")
-		os.Exit(1)
+		log.Fatalln("not in an issue branch")
 	}
-	fmt.Println(github.IssueAsHashtag(issue))
-	client := hub.Authenticate()
-	url, err := octokit.CurrentUserURL.Expand(octokit.M{})
-	if err != nil {
-		panic("Not happen")
+	tmpl := github.IssueAsHashtag(issue)
+	if err := git.CommitWithTemplate(tmpl); err != nil {
+		log.Fatalln(err)
 	}
-	fmt.Println(url)
-	user, result := client.Users(url).One()
-	if result.Err != nil {
-		panic(result.Response.Status)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "hubbit"
+	app.Usage = "custom commands for Git/GitHub"
+	app.Commands = []cli.Command{
+		{
+			Name:    "commit",
+			Aliases: []string{"c"},
+			Usage:   "record changes to the git repository with a hashtag",
+			Action:  commit,
+		},
 	}
-	fmt.Println(user.ReposURL)
+
+	app.Run(os.Args)
 }
